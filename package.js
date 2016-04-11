a34273fc1993e079dd0a85d1a84eab9de7054405
Reio Piller <reio@heathmont.net>
2016-04-11 15:16:04 +0300

    added mongo dependency

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Custom pub/sub interface for Meteor on top of Redis',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/chatr/redpubsub.git',
+  git: 'https://github.com/hypno2000/redpubsub.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -19,7 +19,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
 
   api.use(['random', 'underscore', 'ddp-server', 'ejson']);
-  api.use(['minimongo', 'diff-sequence'], 'server');
+  api.use(['minimongo', 'diff-sequence', 'mongo'], 'server');
 
   api.addFiles('namespace.js');
   api.addFiles(['utils-server.js', 'redis.js'], 'server');
